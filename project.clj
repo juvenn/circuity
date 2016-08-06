6baf0f04b117009c55bbab38be7436e226be52ad
Juvenn Woo <machese@gmail.com>
2016-08-06 16:28:02 +0800

    Bump small version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject circuity/circuity "0.1.0"
+(defproject circuity/circuity "0.1.1"
   :description "A minimal Circuit Breaker in Clojure"
   :url "https://github.com/juvenn/circuity"
   :license {:name "Apache License 2.0"
